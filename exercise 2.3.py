0998199d9b52e21bb42c229a1536d34d552af5c9
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-10 12:19:27 +0700

    exercise 2.3 add

---
+++ exercise 2.3.py
@@ -0,0 +1,10 @@
+import math
+def greet_user():
+    print("Simple program for calculating the area and circumference of a circle")
+
+greet_user()
+jari2 = float(input("masukkan jari-jari: "))
+print(f"luas lingkaran adalah {3.14 * (jari2 ** 2)} centimeter persegi")
+print(f"Keliling Lingkaran adalah {3.14 * 2 * jari2} centimeter")
+
+
