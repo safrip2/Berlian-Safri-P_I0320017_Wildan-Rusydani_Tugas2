78c8518c17b3f1fd8aef971b689216681fb4a345
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-10 12:00:11 +0700

    exercise 2.1 add

---
+++ exercise 2.2.py
@@ -0,0 +1,27 @@
+nama = "Erwin Smith"
+print(nama)
+
+umur = 40
+print(umur)
+type(umur)
+umur = "empat puluh"
+print(umur)
+type(umur)
+
+first_name = "Ramadhan "
+mid_name = "Budi "
+last_name = "Yusuf"
+nama_lengkap = first_name + mid_name + last_name
+umur = 19
+hobi = "Badminton"
+print(f"Biodata \n{nama_lengkap} \numur: {umur} \nhobi: {hobi} ")
+
+#contoh variabel lain
+inivariabel = "Halo"
+ini_variabel = "Hai"
+_inijugavariabel = "Hi"
+inivariabel222 = "oioioi"
+
+panjang = 10
+lebar = 9
+print(f"Luas: {panjang * lebar} squared")
