e34bbb1373ca1433aa19a6ddd4ad60e24931ad77
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-11 22:15:58 +0700

    fix, gak pake while

--- tugas 2.py
+++ tugas 2.py
@@ -1,29 +1,59 @@
 print(
     """============================================================================
-    welcome to your personal assistant, my name is alexa
-    here i will help you to find your past memories
-    cause i know that you had problem to remember something after you had vichile accident
-    You can give me commands by typing
-    1. who am i
-    2. where am i
-    3. what day is it
-    4. etc.
+    Hello There
+    welcome to your personal assistant, my name is Aria
+    you can give me command by typing some numbers
+    0 to show all command
 ============================================================================
     """
 )
-def questionmark():
+def elsee():
     print("anything else?")
-need1 = input("What do you need?")
-while need1.upper() == "WHO AM I":
-    print("Your Name is Berlian Safri Prakoso")
-    questionmark()
-    input()
-elif need1.upper() == "WHERE AM I":
-    print("according to this map, you are in your house, jayakatwang street 60 Madiun")
-elif need1.upper() == "WHAT DAY IS IT":
+need = int(input("what do you need: "))
+if need == 1:
+    print("Your name is Berlian Safri Prakoso")
+    elsee()
+elif need == 2:
+    print("according to this map, you are on Broadgreen Hospital Liverpool")
+    elsee()
+elif need == 3:
     from datetime import date
     today = date.today()
     d2 = today.strftime("%B %d, %Y")
     print("today is ", d2)
-
-
+    elsee()
+elif need == 4:
+    print("you don't remember it? you have no girlfriend, so sad :(")
+    elsee()
+elif need == 5:
+    print("alright, the soup would come in 5 minutes")
+    elsee()
+elif need == 6:
+    print("your height is 173 cm")
+    elsee()
+elif need == 7:
+    from datetime import datetime
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("now is", current_time)
+    elsee()
+elif need == 8:
+    temp = (input("your temperature is 98.6 Fahrenheit, want to see in Celcius?"))
+    if temp.upper() == "YES":
+        print(f"your body temperature is {(98.6 -32) * 0.56 } Celcius")
+        elsee()
+elif need == 0:
+    print(
+        """
+        1 = name
+        2 = location
+        3 = date
+        4 = am i single?
+        5 = order soup
+        6 = height
+        7 = current time
+        8 = temperature
+        """
+    )
+else:
+    print("this feature will bring to you soon")
