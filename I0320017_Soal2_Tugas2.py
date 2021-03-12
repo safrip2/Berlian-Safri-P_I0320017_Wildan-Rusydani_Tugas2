50b9b36756987ec7a16acd1972a89f0c5bbd4fe6
Berlian Safri Prakoso <berliansafrip@gmail.com>
2021-03-12 21:22:59 +0700

    tugas 2 fix adding while

--- I0320017_Soal2_Tugas2.py
+++ I0320017_Soal2_Tugas2.py
@@ -3,58 +3,47 @@ print(
     Hello There
     welcome to your personal assistant, my name is Aria
     you can give me command by typing some numbers
-    0 to show all command
+    type 20 to show all command
+    type 00 to stop the program
 ============================================================================
     """
 )
 def elsee():
     print("anything else?")
-need = int(input("what do you need: "))
-if need == 1:
-    print("Your name is Berlian Safri Prakoso")
-    elsee()
-elif need == 2:
-    print("according to this map, you are on Broadgreen Hospital Liverpool")
-    elsee()
-elif need == 3:
-    from datetime import date
-    today = date.today()
-    d2 = today.strftime("%B %d, %Y")
-    print("today is ", d2)
-    elsee()
-elif need == 4:
-    print("you don't remember it? you have no girlfriend, so sad :(")
-    elsee()
-elif need == 5:
-    print("alright, the soup would come in 5 minutes")
-    elsee()
-elif need == 6:
-    print("your height is 173 cm")
-    elsee()
-elif need == 7:
-    from datetime import datetime
-    tanggal = datetime.now()
-    current_time = tanggal.strftime("%H:%M:%S")
-    print("now is", current_time)
-    elsee()
-elif need == 8:
-    temp = (input("your temperature is 98.6 Fahrenheit, want to see in Celcius?"))
-    if temp.upper() == "YES":
-        print(f"your body temperature is {(98.6 -32) * 0.56 } Celcius")
+need = 0
+while need == 0:
+    need2 = int(input("what do you need? "))
+    need = need * 0
+    if need2 == 1:
+        print("your name is Berlian Safri Prakoso")
         elsee()
-elif need == 0:
-    print(
-        """
-        1 = name
-        2 = location
-        3 = date
-        4 = am i single?
-        5 = order soup
-        6 = height
-        7 = current time
-        8 = temperature
-        """
-    )
-else:
-    print("this feature will bring to you soon")
-
+    elif need2 == 2:
+        print("you are 19 years old")
+        elsee()
+    elif need2 == 3:
+        print("according to this map, you are on Eden Project")
+        elsee()
+    elif need2 == 4:
+        from datetime import date
+        today = date.today()
+        d2 = today.strftime("%B %d, %Y")
+        print("today is ", d2)
+        elsee()
+    elif need2 == 5:
+        print("ordering, Bakso :)")
+        bakso = float(input("how many servings of bakso do you want?"))
+        print(f"the prices for your bakso is Rp.{bakso * 14000} Rupiah")
+        elsee()
+    elif need2 == 20:
+        print("""
+        1. your name
+        2. your age
+        3. location
+        4. date
+        5. order bakso
+        20. show all command
+        00. stop program""")
+        elsee()
+    elif need2 == 00:
+        print("Program Stopped!.\nthank you")
+        break
